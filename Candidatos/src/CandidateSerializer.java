e4a14f59c0dca2616dabe6a666d104b7d7e47740
gabrielmelim <gabrielmelim2012@hotmail.com>
2023-06-21 18:03:02 -0300

    Algoritimo Candidatos Versão 1

--- Candidatos/src/CandidateSerializer.java
+++ Candidatos/src/CandidateSerializer.java
@@ -10,46 +10,46 @@ public class CandidateSerializer {
 
     public static boolean gravarArquivoDelimitado(String caminhoArquivo, List<Candidato> candidatos) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(caminhoArquivo))) {
-            writer.write("Nome");
-            writer.write(";");
-            writer.write("\t\tCpf");
-            writer.write(";");
-            writer.write("\t\tDataNasc");
-            writer.write(";");
-            writer.write("\tSexo");
-            writer.write(";");
-            writer.write("\t\tCelular");
-            writer.write(";");
-            writer.write("\t  Zap");
-            writer.write(";");
-            writer.write("\t  Tel");
-            writer.write(";");
-            writer.write("\t\t\t\tEmail");
-            writer.write(";");
-            writer.write("\t\tEstado");
-            writer.write(";");
-            writer.write("\t\tCidade");
-            writer.write(";");
-            writer.write("\t\tRua");
-            writer.write(";");
-            writer.write("\t\t N°");
-            writer.write("  Bairro");
-            writer.write(";");
-            writer.write("Comp");
-            writer.write(";");
-            writer.write("Emp");
-            writer.write(";");
-            writer.write("Prof");
-            writer.write(";");
-            writer.write("\t\tSal.");
-            writer.write(";");
-            writer.write("eAtual");
-            writer.write(";");
-            writer.write("\tHab");
-            writer.write(";");
-            writer.write("\t Min");
-            writer.write(";");
-            writer.write("\t Max\n");
+//            writer.write("Nome");
+//            writer.write(";");
+//            writer.write("\t\tCpf");
+//            writer.write(";");
+//            writer.write("\t\tDataNasc");
+//            writer.write(";");
+//            writer.write("\tSexo");
+//            writer.write(";");
+//            writer.write("\t\tCelular");
+//            writer.write(";");
+//            writer.write("\t  Zap");
+//            writer.write(";");
+//            writer.write("\t  Tel");
+//            writer.write(";");
+//            writer.write("\t\t\t\tEmail");
+//            writer.write(";");
+//            writer.write("\t\tEstado");
+//            writer.write(";");
+//            writer.write("\t\tCidade");
+//            writer.write(";");
+//            writer.write("\t\tRua");
+//            writer.write(";");
+//            writer.write("\t\t N°");
+//            writer.write("  Bairro");
+//            writer.write(";");
+//            writer.write("Comp");
+//            writer.write(";");
+//            writer.write("Emp");
+//            writer.write(";");
+//            writer.write("Prof");
+//            writer.write(";");
+//            writer.write("\t\tSal.");
+//            writer.write(";");
+//            writer.write("eAtual");
+//            writer.write(";");
+//            writer.write("\tHab");
+//            writer.write(";");
+//            writer.write("\t Min");
+//            writer.write(";");
+//            writer.write("\t Max\n");
 
             for (Candidato candidato : candidatos) {
                 writer.write(formatarCampo(candidato.getNome()));
@@ -118,82 +118,116 @@ public class CandidateSerializer {
     public static boolean gravarArquivoPosicional(String caminhoArquivo, List<Candidato> candidatos) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(caminhoArquivo))) {
             writer.write(campoFormatadoUpperCase("id", 2));
-            adicionarEspacamento(writer,28);
+            adicionarEspacamento(writer, 28);
             writer.write(campoFormatadoUpperCase("nome", 30) + "");
             writer.write(campoFormatadoUpperCase("cpf", 11));
-            adicionarEspacamento(writer,19);
+            adicionarEspacamento(writer, 19);
             writer.write(campoFormatadoUpperCase("data_nascimento", 15));
-            adicionarEspacamento(writer,15);
-            writer.write(campoFormatadoUpperCase("sexo", 4) );
-            adicionarEspacamento(writer,26);
+            adicionarEspacamento(writer, 15);
+            writer.write(campoFormatadoUpperCase("sexo", 4));
+            adicionarEspacamento(writer, 26);
             writer.write(campoFormatadoUpperCase("Logradouro", 10));
-            adicionarEspacamento(writer,40);
+            adicionarEspacamento(writer, 40);
             writer.write(campoFormatadoUpperCase("numero", 6));
-            adicionarEspacamento(writer,30);
+            adicionarEspacamento(writer, 30);
             writer.write(campoFormatadoUpperCase("complemento", 20));
-            adicionarEspacamento(writer,30);
+            adicionarEspacamento(writer, 30);
             writer.write(campoFormatadoUpperCase("bairro", 30));
-            adicionarEspacamento(writer,30);
-            writer.write(campoFormatadoUpperCase("cidade", 30) );
-            adicionarEspacamento(writer,30);
+            adicionarEspacamento(writer, 30);
+            writer.write(campoFormatadoUpperCase("cidade", 30));
+            adicionarEspacamento(writer, 30);
             writer.write(campoFormatadoUpperCase("estado", 30));
-            adicionarEspacamento(writer,30);
+            adicionarEspacamento(writer, 30);
             writer.write(campoFormatadoUpperCase("email", 30));
-            adicionarEspacamento(writer,30);
+            adicionarEspacamento(writer, 30);
             writer.write(campoFormatadoUpperCase("telefone", 30));
-            adicionarEspacamento(writer,30);
+            adicionarEspacamento(writer, 30);
             writer.write(campoFormatadoUpperCase("celular", 30));
-            adicionarEspacamento(writer,30);
-            writer.write(campoFormatadoUpperCase("***_*******_*_*****", 30)+ "\n");
-//            adicionarEspacamento(writer,30);
-//            writer.write(campoFormatadoUpperCase("profissao", 30));
-//            adicionarEspacamento(writer,30);
-//            writer.write(campoFormatadoUpperCase("empresa", 30));
-//            adicionarEspacamento(writer,30);
-//            writer.write(campoFormatadoUpperCase("meu_celular_é_whats", 30)+ "\n");
-//            adicionarEspacamento(writer,30);
-//            writer.write(campoFormatadoUpperCase("salario", 30));
-//            adicionarEspacamento(writer,30);
-//            writer.write(campoFormatadoUpperCase("emprego_atual", 30));
-//            adicionarEspacamento(writer,30);
-//            writer.write(campoFormatadoUpperCase("pretensao_minima", 30));
-//            adicionarEspacamento(writer,30);
-//            writer.write(campoFormatadoUpperCase("pretensao_maxima", 30));
-//            adicionarEspacamento(writer,30);
-//            writer.write(campoFormatadoUpperCase("habilidades", 30)+ "\n");
+            adicionarEspacamento(writer, 30);
+            writer.write(campoFormatadoUpperCase("meu_celular_é_whats", 30));
+            adicionarEspacamento(writer, 30);
+            writer.write(campoFormatadoUpperCase("profissao", 30));
+            adicionarEspacamento(writer, 30);
+            writer.write(campoFormatadoUpperCase("empresa", 30));
+            adicionarEspacamento(writer, 30);
+            writer.write(campoFormatadoUpperCase("salario", 30));
+            adicionarEspacamento(writer, 30);
+            writer.write(campoFormatadoUpperCase("emprego_atual", 30));
+            adicionarEspacamento(writer, 30);
+            writer.write(campoFormatadoUpperCase("pretensao_minima", 30));
+            adicionarEspacamento(writer, 30);
+            writer.write(campoFormatadoUpperCase("pretensao_maxima", 30));
+            adicionarEspacamento(writer, 30);
+            writer.write(campoFormatadoUpperCase("habilidades", 30) + "\n");
 
 
             for (Candidato candidato : candidatos) {
                 writer.write(campoId(candidato.getId(), 2));
-                adicionarEspacamento(writer,28);
+                adicionarEspacamento(writer, 28);
                 writer.write(campoFormatadoUpperCase(candidato.getNome(), 30));
                 writer.write(contemSomenteDigitos(candidato.getCpf(), 11));
-                adicionarEspacamento(writer,19);
-                writer.write(formatarDataPosicional(candidato.getDataNascimento(),8));
-                adicionarEspacamento(writer,21);
-                writer.write(obterCaractereGenero(candidato.getSexo(),1));
-                adicionarEspacamento(writer,28);
-                writer.write(campoFormatadoUpperCase(candidato.getEndereco().getLogradouro(),40));
-                adicionarEspacamento(writer,10);
-                writer.write(preencherComZerosEsquerda(candidato.getEndereco().getNumero(),5));
-                adicionarEspacamento(writer,31);
-                writer.write(campoFormatadoUpperCase(candidato.getEndereco().getComplemento(),20));
-                adicionarEspacamento(writer,30);
-                writer.write(campoFormatadoUpperCase(candidato.getEndereco().getBairro(),30));
-                adicionarEspacamento(writer,30);
-                writer.write(campoFormatadoUpperCase(candidato.getEndereco().getCidade(),40));
-                adicionarEspacamento(writer,20);
-                writer.write(campoFormatadoUpperCase(candidato.getEndereco().getEstado(),10));
-                adicionarEspacamento(writer,50);
-                writer.write(campoFormatadoLowerCase(candidato.getContato().getEmail(),50));
-                adicionarEspacamento(writer,10);
-                writer.write(formatarNumeroTelefone(candidato.getContato().getTelefone(),10));
-                adicionarEspacamento(writer,48);
-                writer.write(formatarNumeroTelefone(candidato.getContato().getCelular(),11));
+                adicionarEspacamento(writer, 19);
+                writer.write(formatarDataPosicional(candidato.getDataNascimento(), 8));
+                adicionarEspacamento(writer, 21);
+                writer.write(obterCaractereGenero(candidato.getSexo(), 1));
+                adicionarEspacamento(writer, 28);
+                writer.write(campoFormatadoUpperCase(candidato.getEndereco().getLogradouro(), 40));
+                adicionarEspacamento(writer, 10);
+                writer.write(preencherComZerosEsquerda(candidato.getEndereco().getNumero(), 5));
+                adicionarEspacamento(writer, 31);
+                writer.write(campoFormatadoUpperCase(candidato.getEndereco().getComplemento(), 20));
+                adicionarEspacamento(writer, 30);
+                writer.write(campoFormatadoUpperCase(candidato.getEndereco().getBairro(), 30));
+                adicionarEspacamento(writer, 30);
+                writer.write(campoFormatadoUpperCase(candidato.getEndereco().getCidade(), 40));
+                adicionarEspacamento(writer, 20);
+                writer.write(campoFormatadoUpperCase(candidato.getEndereco().getEstado(), 10));
+                adicionarEspacamento(writer, 50);
+                writer.write(campoFormatadoLowerCase(candidato.getContato().getEmail(), 50));
+                adicionarEspacamento(writer, 10);
+                writer.write(formatarNumeroTelefone(candidato.getContato().getTelefone(), 10));
+                adicionarEspacamento(writer, 48);
+                writer.write(formatarNumeroTelefone(candidato.getContato().getCelular(), 11));
+                adicionarEspacamento(writer, 49);
+                writer.write(formatarNumeroTelefoneWpp(candidato.getContato().getCelular(), 1, candidato.getContato().isCelularWhatsApp()));
+                adicionarEspacamento(writer, 57);
+                writer.write(campoFormatadoUpperCase(candidato.getExperienciaProfissional().getProfissao(), 30));
+                adicionarEspacamento(writer, 30);
+                writer.write(campoFormatadoUpperCase(candidato.getExperienciaProfissional().getEmpresa(), 30));
+                adicionarEspacamento(writer, 30);
+                writer.write(formatarSalario(candidato.getExperienciaProfissional().getSalario(), 10));
+                adicionarEspacamento(writer, 50);
+                writer.write(formatarCampoLogicoPosicional(candidato.getExperienciaProfissional().isEmpregoAtual(), 1));
+                adicionarEspacamento(writer, 59);
+                writer.write(formatarSalario(candidato.getFaixaSalarial().getValorMinimo(), 10));
+                adicionarEspacamento(writer, 50);
+                writer.write(formatarSalario(candidato.getFaixaSalarial().getValorMaximo(), 10));
+                adicionarEspacamento(writer, 50);
 
+                List<String> habilidades = candidato.getHabilidades();
+                if (habilidades.isEmpty()) {
+                    writer.write("VAZIO");
+                } else {
+                    int tamanhoMaximo = 50; // Defina o tamanho máximo desejado aqui
+                    StringBuilder habilidadesFormatadas = new StringBuilder();
+                    for (int i = 0; i < habilidades.size(); i++) {
+                        String habilidade = habilidades.get(i);
+                        String habilidadeFormatada = habilidade.length() > tamanhoMaximo ? habilidade.substring(0, tamanhoMaximo) : habilidade;
+                        habilidadesFormatadas.append(habilidadeFormatada);
+                        if (i < habilidades.size() - 1) {
+                            habilidadesFormatadas.append(",");
+                        }
+                    }
+                    String habilidadesString = habilidadesFormatadas.toString();
+                    if (habilidadesString.length() < tamanhoMaximo) {
+                        habilidadesString = String.format("%-" + tamanhoMaximo + "s", habilidadesString);
+                    }
+                    writer.write(habilidadesString);
+                }
 
 
                 writer.newLine();
+
             }
 
             return true;
@@ -212,6 +246,7 @@ public class CandidateSerializer {
         }
         return valor;
     }
+
     public static void adicionarEspacamento(BufferedWriter writer, int tamanho) {
         try {
             for (int i = 0; i < tamanho; i++) {
@@ -225,6 +260,15 @@ public class CandidateSerializer {
 
     public static String formatarCampoLogico(boolean valor) {
         return valor ? "true" : "false";
+    }
+
+    public static String formatarCampoLogicoPosicional(Boolean valor, int tamanho) {
+        if (valor == null) {
+            return String.format("%" + tamanho + "s", ""); // Retorna um espaço em branco se o valor for nulo
+        } else {
+            String str = valor.toString();
+            return str.substring(0, 1).toUpperCase();
+        }
     }
 
     private static String formatarEnum(Enum<?> valor) {
@@ -366,7 +410,11 @@ public class CandidateSerializer {
 
     public static String formatarNumeroTelefone(String numero, int tamanho) {
         if (numero == null || numero.isEmpty()) {
-            return String.format("%-" + tamanho + "s", "0000000000");
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < tamanho; i++) {
+                sb.append('0');
+            }
+            return sb.toString();
         }
 
         StringBuilder sb = new StringBuilder();
@@ -377,15 +425,70 @@ public class CandidateSerializer {
         }
 
         if (sb.length() == 0) {
-            return String.format("%-" + tamanho + "s", "0000000000");
+            StringBuilder emptySb = new StringBuilder();
+            for (int i = 0; i < tamanho; i++) {
+                emptySb.append('0');
+            }
+            return emptySb.toString();
         }
 
         try {
             long num = Long.parseLong(sb.toString());
             return String.format("%0" + tamanho + "d", num);
         } catch (NumberFormatException e) {
-            return String.format("%-" + tamanho + "s", "0000000000");
+            StringBuilder errorSb = new StringBuilder();
+            for (int i = 0; i < tamanho; i++) {
+                errorSb.append('0');
+            }
+            return errorSb.toString();
         }
+    }
+
+    public static String formatarNumeroTelefoneWpp(String numero, int tamanho, boolean isWhatsApp) {
+        if (numero == null || numero.isEmpty()) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < tamanho; i++) {
+                sb.append('0');
+            }
+            return sb.toString();
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (char c : numero.toCharArray()) {
+            if (Character.isDigit(c)) {
+                sb.append(c);
+            }
+        }
+
+        if (sb.length() == 0) {
+            StringBuilder emptySb = new StringBuilder();
+            for (int i = 0; i < tamanho; i++) {
+                emptySb.append('0');
+            }
+            return emptySb.toString();
+        }
+
+        if (isWhatsApp) {
+            return isWhatsApp ? "T" : "F";
+        }
+
+        try {
+            long num = Long.parseLong(sb.toString());
+            return String.format("%0" + tamanho + "d", num);
+        } catch (NumberFormatException e) {
+            StringBuilder errorSb = new StringBuilder();
+            for (int i = 0; i < tamanho; i++) {
+                errorSb.append('0');
+            }
+            return errorSb.toString();
+        }
+    }
+
+    public static String formatarSalario(double salario, int tamanho) {
+        String salarioString = String.valueOf(salario);
+        salarioString = salarioString.replaceAll("[^0-9]", ""); // Remove todos os caracteres não numéricos
+        salarioString = salarioString.length() < tamanho ? String.format("%0" + tamanho + "d", Long.parseLong(salarioString)) : salarioString;
+        return salarioString;
     }
 
 }
